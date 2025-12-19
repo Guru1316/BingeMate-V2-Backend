db5edcdbc2a87933ac0198d68c7477fc5368b47f
Guru1316 <guruprasadgdr1@gmail.com>
2025-12-19 09:57:22 +0530

    Added Origin

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:3000", "https://guru1316.github.io"],
+    origin: ["http://localhost:5173", "http://localhost:3000", "https://guru1316.github.io", "https://binge-mate-frontend.vercel.app"],
     credentials: true
 }));
 
